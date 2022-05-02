d01156530f104b3724c5b2f3d1a0adcbebae5e4c
Deepana Naidu <deepana.naidu@gmail.com>
2022-05-02 23:23:44 +0100

    Add getListOfSeatsAllocated() && listOfSeatsAllocated() methods to print seats list

--- src/main/java/CinnamonCinemas.java
+++ src/main/java/CinnamonCinemas.java
@@ -6,7 +6,6 @@ public class CinnamonCinemas {
     private final boolean[][] seatMap;
     private int totalAvailableSeats;
 
-
     public CinnamonCinemas(int totalAvailableSeats) {
         this.totalAvailableSeats = totalAvailableSeats;
         allocatedSeatList = new ArrayList<>();
@@ -22,6 +21,7 @@ public class CinnamonCinemas {
                 if(!seatMap[row][seat] && seatsRequested > 0){
                     seatMap[row][seat] = true;
                     ableToAllocate = true;
+                    listOfSeatsAllocated(row,seat);
                     totalAvailableSeats--;
                     seatsRequested--;
                 }
@@ -29,5 +29,17 @@ public class CinnamonCinemas {
             if(seatsRequested == 0) break;
         }
         return ableToAllocate && totalAvailableSeats >= 0;
+    }
+
+    private void listOfSeatsAllocated(int row, int seat){
+        StringBuilder sb = new StringBuilder();
+        if(row == 0) sb.append("A").append(seat+1);
+        else if(row == 1) sb.append("B").append(seat+1);
+        else if(row == 2) sb.append("C").append(seat+1);
+        allocatedSeatList.add(sb.toString());
+    }
+
+    public List<String> getListOfSeatsAllocated(){
+        return allocatedSeatList;
     }
 }
