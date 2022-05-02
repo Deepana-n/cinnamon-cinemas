f4544a893eeb04cb57e9fddcff2ec72f3569f995
Deepana Naidu <deepana.naidu@gmail.com>
2022-05-02 20:15:13 +0100

    Parameterized constructor and allocateSeats() method added

--- src/main/java/CinnamonCinemas.java
+++ src/main/java/CinnamonCinemas.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CinnamonCinemas {
+    private final List<String> allocatedSeatList;
+    private final boolean[][] seatMap;
+    private int totalAvailableSeats;
+
+
+    public CinnamonCinemas(int totalAvailableSeats) {
+        this.totalAvailableSeats = totalAvailableSeats;
+        allocatedSeatList = new ArrayList<>();
+        seatMap =  new boolean[][]{{false, false, false, false, false},{false, false, false, false, false},
+                {false, false, false, false, false}};
+    }
+
+    public boolean allocateSeats(int seatsRequested) {
+        boolean ableToAllocate = false;
+        for(int row = 0; row < seatMap.length; row++){
+            for(int seat = 0; seat < seatMap[0].length; seat++){
+                if(!seatMap[row][seat] && seatsRequested > 0){
+                    seatMap[row][seat] = true;
+                    ableToAllocate = true;
+                    totalAvailableSeats--;
+                    seatsRequested--;
+                }
+            }
+            if(seatsRequested == 0) break;
+        }
+        return ableToAllocate && totalAvailableSeats >= 0;
+    }
 }
