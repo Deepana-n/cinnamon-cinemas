b0fea67e57a7408e509cca01f735c4e4bef113f8
Deepana Naidu <deepana.naidu@gmail.com>
2022-05-02 20:14:34 +0100

    test method checkSuccessfulAllocation() added

--- src/test/java/CinnamonCinemasTest.java
+++ src/test/java/CinnamonCinemasTest.java
@@ -1,5 +1,20 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CinnamonCinemasTest {
+    private CinnamonCinemas cinemaObj;
+
+    @BeforeEach
+    public void setUp(){
+        cinemaObj = new CinnamonCinemas(15);
+    }
+
+    @Test
+    public void checkSuccessfulAllocation(){
+        int seatsRequested = 1;
+        assertTrue(cinemaObj.allocateSeats(seatsRequested));
+    }
 
 }
