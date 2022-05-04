f961703513ab1371d5dc0741133eee8ae7646244
Deepana Naidu <deepana.naidu@gmail.com>
2022-05-04 13:52:25 +0100

    added checkValidMultipleSeatAllocation() and checkInvalidMultipleSeatAllocation test methods to test if allocateSeats() method shows accurate result when called multiple times

--- src/test/java/CinnamonCinemasTest.java
+++ src/test/java/CinnamonCinemasTest.java
@@ -31,6 +31,40 @@ class CinnamonCinemasTest {
         assertFalse(cinemaObj.allocateSeats(seatsRequested));
     }
 
+    @ParameterizedTest(name = "{index}) For multiple seat requests: \"{0}\"")
+    @CsvSource(textBlock = """
+            1 2 1,
+            2 2 2 3 3 3,
+            1 2 3 1 2 3 1 1 1
+            """)
+    public void checkValidMultipleSeatAllocation(String stringOfSeatsRequested) {
+        cinemaObj = new CinnamonCinemas(15);
+        String[] seatsRequested = stringOfSeatsRequested.split(" ");
+        for (String s : seatsRequested) {
+            assertTrue(cinemaObj.allocateSeats(Integer.parseInt(s)));
+        }
+    }
+
+    @ParameterizedTest(name = "{index}) For multiple seats of: \"{0}\"")
+    @CsvSource(textBlock = """
+            3 3 3 3 3 3,
+            2 2 2 3 3 3 2,
+            1 2 3 1 2 3 1 1 1 2
+            """)
+    public void checkInvalidMultipleSeatAllocation(String stringOfSeatsRequested) {
+        cinemaObj = new CinnamonCinemas(15);
+        int countAllocatedSeats = 15;
+        String[] seatsRequested = stringOfSeatsRequested.split(" ");
+        for (String s : seatsRequested) {
+            countAllocatedSeats -= Integer.parseInt(s);
+            if(countAllocatedSeats <= 15 && countAllocatedSeats >=0){
+                assertTrue(cinemaObj.allocateSeats(Integer.parseInt(s)));
+            }else{
+                assertFalse(cinemaObj.allocateSeats(Integer.parseInt(s)));
+            }
+        }
+    }
+
     @Disabled
     @ParameterizedTest(name = "{index}) For seats: \"{0}\" the outcome is \"{1}\"")
     @CsvSource(textBlock = """
