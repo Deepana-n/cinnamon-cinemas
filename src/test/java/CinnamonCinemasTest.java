17cb1a2e45d638a7acdd2b96f72912a847416fac
Deepana Naidu <deepana.naidu@gmail.com>
2022-05-04 13:32:41 +0100

    Renamed and changed logic of checkValidSeatSingleAllocation()checkInvalidSeatSingleAllocation() after adding validation checks in allocateSeats() method

--- src/test/java/CinnamonCinemasTest.java
+++ src/test/java/CinnamonCinemasTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -17,19 +18,20 @@ class CinnamonCinemasTest {
     }
 
     @ParameterizedTest(name = "{index}) For seats: \"{0}\"")
-    @ValueSource(ints = {1, 2, 3, 14, 15})
-    public void checkValidSeatAllocation(int seatsRequested) {
+    @ValueSource(ints = {1, 2, 3})
+    public void checkValidSeatSingleAllocation(int seatsRequested) {
         cinemaObj = new CinnamonCinemas(15);
         assertTrue(cinemaObj.allocateSeats(seatsRequested));
     }
 
     @ParameterizedTest(name = "{index}) For seats: \"{0}\"")
-    @ValueSource(ints = {0, 16, 18})
-    public void checkInvalidSeatAllocation(int seatsRequested) {
+    @ValueSource(ints = {0, 12, 15})
+    public void checkInvalidSeatSingleAllocation(int seatsRequested) {
         cinemaObj = new CinnamonCinemas(15);
         assertFalse(cinemaObj.allocateSeats(seatsRequested));
     }
 
+    @Disabled
     @ParameterizedTest(name = "{index}) For seats: \"{0}\" the outcome is \"{1}\"")
     @CsvSource(textBlock = """
             2,  A1 A2,
@@ -43,6 +45,4 @@ class CinnamonCinemasTest {
         cinemaObj.allocateSeats(seatsRequested);
         assertEquals(expectedResultList,cinemaObj.getListOfSeatsAllocated());
     }
-
-
 }
